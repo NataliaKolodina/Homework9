ccf51009aa1e73ddbf83c480da4db5fd10165704
«Natalia <kolodina_melbyk@mail.ru>
2023-02-24 12:17:54 +0400

    add task64

---
+++ task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N.
+// Напишите программу, которая выведет все натуральные четные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.Write("Введите число: ");
+string n = Console.ReadLine()!;
+
+Console.Write($"N = {n} -> ");
+Console.WriteLine(NaturalNumbers(n));
+string NaturalNumbers(string num, string start = "1")
+{
+    if (num == start)
+    {
+        return num + ".";
+    }
+    else
+    {
+        return num + ", " + NaturalNumbers(Convert.ToString(int.Parse(num) - 1));
+    }
+}
